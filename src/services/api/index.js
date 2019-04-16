3d7b95569e0c4507c599658792b9ae9c0bcd278f
Henry Blevins <heblevi@gmail.com>
2019-04-16 13:26:24 -0400

    Add api requests for projects, roles and classrooms

--- src/services/api/index.js
+++ src/services/api/index.js
@@ -24,3 +24,58 @@ export const register = ({name, email, password}) => {
 export const login = ({email, password}) => {
   return axios.put(`${API_URL}/auth/login`, {email, password});
 };
+
+// projects
+export const createProject = ({name, description}) => {
+  return axiosWithAuth.post(`${API_URL}/projects/`, {name, description});
+};
+
+export const getProjects = ({name, description}) => {
+  return axiosWithAuth.get(`${API_URL}/projects/`, {name, description});
+};
+
+// roles
+export const createRole = ({name}) => {
+  return axiosWithAuth.post(`${API_URL}/roles/`, {name});
+};
+
+export const getRoles = () => {
+  return axiosWithAuth.get(`${API_URL}/roles/`);
+};
+
+export const updateRole = (id, {name}) => {
+  return axiosWithAuth.put(`${API_URL}/roles/${id}`, {name});
+};
+
+// classrooms
+export const createClassroom = ({name}) => {
+  return axiosWithAuth.post(`${API_URL}/classrooms`, {name});
+};
+
+export const editClassroom = (id, {name}) => {
+  return axiosWithAuth.put(`${API_URL}/classrooms/${id}`, {name});
+};
+
+export const getClassroom = (id) => {
+  return axiosWithAuth.get(`${API_URL}/classrooms/${id}`);
+};
+
+export const getClassrooms = () => {
+  return axiosWithAuth.get(`${API_URL}/classrooms/`);
+};
+
+export const addProjectToClassroom = (classroom_id, {project_id}) => {
+  return axiosWithAuth.post(`${API_URL}/classrooms/${classroom_id}`, {project_id});
+};
+
+export const getClasssroomProject = (classroom_id, classroom_project_id) => {
+  return axiosWithAuth
+    .get(`${API_URL}/classrooms/${classroom_id}/projects/${classroom_project_id}`);
+};
+
+export const createMemberSlot = (classroom_id, classroom_project_id, {role_id}) => {
+  return axiosWithAuth
+    .post(`${API_URL}/classrooms/${classroom_id}/projects/${classroom_project_id}/project_member`,
+          {role_id});
+};
+
